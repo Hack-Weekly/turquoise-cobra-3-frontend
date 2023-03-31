180498311ee6e47a6d28bc958a1128b34062401a
waqarulhaq129 <waqarulhaq129@gmail.com>
2023-04-01 01:14:53 +0500

    added article page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import React,{useEffect} from "react";
+import { Link } from "react-router-dom";
 import "../server/server"
+
 
 
 export default function Home(){
@@ -23,10 +25,10 @@ export default function Home(){
                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{van.name}</h5>
                 
                     <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{van.description}.</p>
-                    <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    <Link to={`/blogs/${van.id}`} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     Read more
                     <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                    </a>
+                    </Link>
                 </div>
         </div>
         )
@@ -34,7 +36,7 @@ export default function Home(){
    
     return(  <div>
              <h1>This is Home</h1> 
-             <div class="flex-auto container max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12">{vansCards}</div>
+                 <div className="flex-auto container max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12">{vansCards}</div>
              </div>       
         )
 
