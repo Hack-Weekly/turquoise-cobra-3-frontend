9f8c7de5760905f296668a29cc49491071bbb4bc
Dera <deraidos@zoho.com>
2023-04-03 01:40:06 +0700

    style: changed flex to grid for consistency

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -3,10 +3,11 @@ import AboutCard from "../components/AboutCard";
 
 export default function TheTeam() {
   return (
-    <div className="flex flex-col items-center ">
+    <div className="flex flex-col items-center">
       <h1 className="mb-8">The Team</h1>
 
-      <div className="flex flex-col flex-wrap w-full justify-center md:flex-row gap-4">
+      {/* <div className="flex flex-col flex-wrap w-full justify-center md:flex-row gap-4"> */}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         <AboutCard />
       </div>
     </div>
